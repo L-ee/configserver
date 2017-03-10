c3a0393967716d218e7aa6ee39dda2bd13e42905
Lee <646135832@qq.com>
2017-03-10 18:31:39 +0800

    initialize

--- src/main/java/demo/ConfigServerApplication.java
+++ src/main/java/demo/ConfigServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
